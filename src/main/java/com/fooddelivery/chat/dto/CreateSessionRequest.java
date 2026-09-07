cb8d54bfb4134c64a10d8930e1b6ce5f0bcf47f7
Parthu <parthudhanireddy@gmail.com>
2026-09-07 13:32:08 +0530

    chore: fix CI timeouts and configure resilience4j/feign

--- src/main/java/com/fooddelivery/chat/dto/CreateSessionRequest.java
+++ src/main/java/com/fooddelivery/chat/dto/CreateSessionRequest.java
@@ -3,128 +3,19 @@ package com.fooddelivery.chat.dto;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import java.util.List;
+import java.util.List;@lombok.AllArgsConstructor
+@lombok.NoArgsConstructor
+@lombok.Data
+@lombok.Builder
+
 
 public class CreateSessionRequest {
     @NotBlank
+    @com.fasterxml.jackson.annotation.JsonProperty(required = true)
     private String orderId;
     @NotEmpty
     @Valid
     private List<ParticipantDto> participants;
 
 
-    @java.lang.SuppressWarnings("all")
-    public static class CreateSessionRequestBuilder {
-        @java.lang.SuppressWarnings("all")
-        private String orderId;
-        @java.lang.SuppressWarnings("all")
-        private List<ParticipantDto> participants;
-
-        @java.lang.SuppressWarnings("all")
-        CreateSessionRequestBuilder() {
-        }
-
-        /**
-         * @return {@code this}.
-         */
-        @java.lang.SuppressWarnings("all")
-        public CreateSessionRequest.CreateSessionRequestBuilder orderId(final String orderId) {
-            this.orderId = orderId;
-            return this;
-        }
-
-        /**
-         * @return {@code this}.
-         */
-        @java.lang.SuppressWarnings("all")
-        public CreateSessionRequest.CreateSessionRequestBuilder participants(final List<ParticipantDto> participants) {
-            this.participants = participants;
-            return this;
-        }
-
-        @java.lang.SuppressWarnings("all")
-        public CreateSessionRequest build() {
-            return new CreateSessionRequest(this.orderId, this.participants);
-        }
-
-        @java.lang.Override
-        @java.lang.SuppressWarnings("all")
-        public java.lang.String toString() {
-            return "CreateSessionRequest.CreateSessionRequestBuilder(orderId=" + this.orderId + ", participants=" + this.participants + ")";
-        }
-    }
-
-    @java.lang.SuppressWarnings("all")
-    public static CreateSessionRequest.CreateSessionRequestBuilder builder() {
-        return new CreateSessionRequest.CreateSessionRequestBuilder();
-    }
-
-    @java.lang.SuppressWarnings("all")
-    public String getOrderId() {
-        return this.orderId;
-    }
-
-    @java.lang.SuppressWarnings("all")
-    public List<ParticipantDto> getParticipants() {
-        return this.participants;
-    }
-
-    @java.lang.SuppressWarnings("all")
-    public void setOrderId(final String orderId) {
-        this.orderId = orderId;
-    }
-
-    @java.lang.SuppressWarnings("all")
-    public void setParticipants(final List<ParticipantDto> participants) {
-        this.participants = participants;
-    }
-
-    @java.lang.Override
-    @java.lang.SuppressWarnings("all")
-    public boolean equals(final java.lang.Object o) {
-        if (o == this) return true;
-        if (!(o instanceof CreateSessionRequest)) return false;
-        final CreateSessionRequest other = (CreateSessionRequest) o;
-        if (!other.canEqual((java.lang.Object) this)) return false;
-        final java.lang.Object this$orderId = this.getOrderId();
-        final java.lang.Object other$orderId = other.getOrderId();
-        if (this$orderId == null ? other$orderId != null : !this$orderId.equals(other$orderId)) return false;
-        final java.lang.Object this$participants = this.getParticipants();
-        final java.lang.Object other$participants = other.getParticipants();
-        if (this$participants == null ? other$participants != null : !this$participants.equals(other$participants)) return false;
-        return true;
-    }
-
-    @java.lang.SuppressWarnings("all")
-    protected boolean canEqual(final java.lang.Object other) {
-        return other instanceof CreateSessionRequest;
-    }
-
-    @java.lang.Override
-    @java.lang.SuppressWarnings("all")
-    public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        final java.lang.Object $orderId = this.getOrderId();
-        result = result * PRIME + ($orderId == null ? 43 : $orderId.hashCode());
-        final java.lang.Object $participants = this.getParticipants();
-        result = result * PRIME + ($participants == null ? 43 : $participants.hashCode());
-        return result;
-    }
-
-    @java.lang.Override
-    @java.lang.SuppressWarnings("all")
-    public java.lang.String toString() {
-        return "CreateSessionRequest(orderId=" + this.getOrderId() + ", participants=" + this.getParticipants() + ")";
-    }
-
-    @java.lang.SuppressWarnings("all")
-    public CreateSessionRequest() {
-    }
-
-    @java.lang.SuppressWarnings("all")
-    public CreateSessionRequest(final String orderId, final List<ParticipantDto> participants) {
-        this.orderId = orderId;
-        this.participants = participants;
-    }
 }
--- src/main/java/com/fooddelivery/chat/dto/SendMessageRequest.java
+++ src/main/java/com/fooddelivery/chat/dto/SendMessageRequest.java
@@ -1,4 +1,8 @@
-package com.fooddelivery.chat.dto;
+package com.fooddelivery.chat.dto;@lombok.AllArgsConstructor
+@lombok.NoArgsConstructor
+@lombok.Data
+@lombok.Builder
+
 
 
 public class SendMessageRequest {
@@ -6,118 +10,4 @@ public class SendMessageRequest {
     private String messageType;
 
 
-    @java.lang.SuppressWarnings("all")
-    public static class SendMessageRequestBuilder {
-        @java.lang.SuppressWarnings("all")
-        private String content;
-        @java.lang.SuppressWarnings("all")
-        private String messageType;
-
-        @java.lang.SuppressWarnings("all")
-        SendMessageRequestBuilder() {
-        }
-
-        /**
-         * @return {@code this}.
-         */
-        @java.lang.SuppressWarnings("all")
-        public SendMessageRequest.SendMessageRequestBuilder content(final String content) {
-            this.content = content;
-            return this;
-        }
-
-        /**
-         * @return {@code this}.
-         */
-        @java.lang.SuppressWarnings("all")
-        public SendMessageRequest.SendMessageRequestBuilder messageType(final String messageType) {
-            this.messageType = messageType;
-            return this;
-        }
-
-        @java.lang.SuppressWarnings("all")
-        public SendMessageRequest build() {
-            return new SendMessageRequest(this.content, this.messageType);
-        }
-
-        @java.lang.Override
-        @java.lang.SuppressWarnings("all")
-        public java.lang.String toString() {
-            return "SendMessageRequest.SendMessageRequestBuilder(content=" + this.content + ", messageType=" + this.messageType + ")";
-        }
-    }
-
-    @java.lang.SuppressWarnings("all")
-    public static SendMessageRequest.SendMessageRequestBuilder builder() {
-        return new SendMessageRequest.SendMessageRequestBuilder();
-    }
-
-    @java.lang.SuppressWarnings("all")
-    public String getContent() {
-        return this.content;
-    }
-
-    @java.lang.SuppressWarnings("all")
-    public String getMessageType() {
-        return this.messageType;
-    }
-
-    @java.lang.SuppressWarnings("all")
-    public void setContent(final String content) {
-        this.content = content;
-    }
-
-    @java.lang.SuppressWarnings("all")
-    public void setMessageType(final String messageType) {
-        this.messageType = messageType;
-    }
-
-    @java.lang.Override
-    @java.lang.SuppressWarnings("all")
-    public boolean equals(final java.lang.Object o) {
-        if (o == this) return true;
-        if (!(o instanceof SendMessageRequest)) return false;
-        final SendMessageRequest other = (SendMessageRequest) o;
-        if (!other.canEqual((java.lang.Object) this)) return false;
-        final java.lang.Object this$content = this.getContent();
-        final java.lang.Object other$content = other.getContent();
-        if (this$content == null ? other$content != null : !this$content.equals(other$content)) return false;
-        final java.lang.Object this$messageType = this.getMessageType();
-        final java.lang.Object other$messageType = other.getMessageType();
-        if (this$messageType == null ? other$messageType != null : !this$messageType.equals(other$messageType)) return false;
-        return true;
-    }
-
-    @java.lang.SuppressWarnings("all")
-    protected boolean canEqual(final java.lang.Object other) {
-        return other instanceof SendMessageRequest;
-    }
-
-    @java.lang.Override
-    @java.lang.SuppressWarnings("all")
-    public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        final java.lang.Object $content = this.getContent();
-        result = result * PRIME + ($content == null ? 43 : $content.hashCode());
-        final java.lang.Object $messageType = this.getMessageType();
-        result = result * PRIME + ($messageType == null ? 43 : $messageType.hashCode());
-        return result;
-    }
-
-    @java.lang.Override
-    @java.lang.SuppressWarnings("all")
-    public java.lang.String toString() {
-        return "SendMessageRequest(content=" + this.getContent() + ", messageType=" + this.getMessageType() + ")";
-    }
-
-    @java.lang.SuppressWarnings("all")
-    public SendMessageRequest() {
-    }
-
-    @java.lang.SuppressWarnings("all")
-    public SendMessageRequest(final String content, final String messageType) {
-        this.content = content;
-        this.messageType = messageType;
-    }
 }
